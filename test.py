5fc72e38c16be225a886225db739045d95c543a1
PengYe <sysuyepeng@gmail.com>
2020-06-16 15:47:55 +0800

    test commit

---
+++ test.py
@@ -0,0 +1,28 @@
+import os
+
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+
+print('helleo world!')
+msg = 'hello'
+
+msg.capitalize()
+msg.split()
+
+# a = np.array()
+a = 1 + 1
+a = 2 - 1 * 0
+c = 2 * 1 + 2 + 1 * 1 - 1
+os.getcwd()
+print(np.array([2, 4, 12]))
+
+print(pd.__version__)
+print(a)
+print('c is :', c)
+
+# print(ddd)
+
+
+def method(a=0):
+    return None
